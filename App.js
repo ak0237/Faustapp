fdba0399304f083dd53266b4ccdefd8a8bd8b715
Andrey <zabuzamomochi237@gmail.com>
2021-09-18 22:26:14 -0300

    Primeiro

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView,  StyleSheet, Text } from 'react-native';
+import {useFonts, Lobster_400Regular} from  '@expo-google-fonts/lobster';
+
+import Cesta from './src/telas/Cesta';
 
 export default function App() {
+  const fonteCarregada = Lobster_400Regular;
+
+  if (!fonteCarregada) {
+    return <View />
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+      
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
